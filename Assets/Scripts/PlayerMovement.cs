f22fcda5600dbfde2f694fe40fbc57952261225f
hannah-nanadev <hannahkellett2@gmail.com>
2023-10-14 13:17:40 +0100

    Basic movement added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    public float speed = 10f;
+    public Rigidbody2D body;
+    private Vector2 moveDirection;
+    private Animator anim;
+    private bool facingDown = true;
+    private bool facingRight = false;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
+    //Update is called once per frame
     void Update()
     {
-        
+        ProcessInputs();
     }
+
+    // FixedUpdate is called once per fixed framerate frame
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX * speed, moveY * speed);
+    }
+
+    void Move()
+    {
+        body.velocity = moveDirection;
+    }
+
 }
